99b06e0d7f06b72c78fbd55bbacb197e227c4035
FengJie <2109921940@qq.com>
2020-07-26 23:57:50 +0800

    1. 定义common基础依赖模块 2. 定义core核心模块

---
+++ staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/constant/RequestPath.java
@@ -0,0 +1,16 @@
+package mall.staringpoint.core.constant;
+
+/**
+ * Feign 接口调用地址
+ *
+ * @Author: FengJie
+ * @Date: 2020/7/26 16:47
+ */
+public interface RequestPath {
+
+    //product部分
+
+    String PRODUCT_QUERY_BY_IDS = "/s/api/product/1/query";
+
+    //order部分
+}
---
+++ staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/tool/util/SessionUtils.java
@@ -0,0 +1,27 @@
+package mall.staringpoint.core.tool.util;
+
+import com.framework.core.base.UserSession;
+import lombok.experimental.UtilityClass;
+
+/**
+ * @Author: FengJie
+ * @Date: 2020/7/26 17:46
+ * @Description: session工具类
+ */
+@UtilityClass
+public class SessionUtils {
+
+    private static final ThreadLocal<UserSession> USER_SESSION_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static UserSession get() {
+        return USER_SESSION_THREAD_LOCAL.get();
+    }
+
+    public static void set(UserSession session) {
+        USER_SESSION_THREAD_LOCAL.set(session);
+    }
+
+    public static void remove() {
+        USER_SESSION_THREAD_LOCAL.remove();
+    }
+}
