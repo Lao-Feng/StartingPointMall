1285eb94d3ac4a643e5e0d5a843c172d51916882
FengJie <2109921940@qq.com>
2020-07-28 23:32:36 +0800

    1.  完善mysql组件依赖

---
+++ staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package mall.staringpoint.core.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import mall.staringpoint.core.base.Result;
+import mall.staringpoint.core.base.Results;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 异常捕获处理
+ * @author FengJie
+ */
+@Slf4j
+@Component
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(ServiceException.class)
+    public Result handle(ServiceException e) {
+        log.error("发生:[Service异常],异常码:{},异常描述:{},", e.getErrorCode(), e.getErrorMsg(), e);
+        return Results.fail(e);
+    }
+}
