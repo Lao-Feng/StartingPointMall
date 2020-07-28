d28e4a8397aa38df7e7d224c90c0115e00d5cfe1
FengJie <2109921940@qq.com>
2020-07-28 23:38:32 +0800

    1.  swagger 路径修复

--- staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/config/SwaggerAutoConfiguration.java
+++ staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/config/SwaggerAutoConfiguration.java
@@ -25,7 +25,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableConfigurationProperties
 public class SwaggerAutoConfiguration {
 
-    private static final String BASE_PACKAGE = "com.framework";
+    private static final String BASE_PACKAGE = "mall.starting";
     private static final String VERSION = "1.0";
 
     @Bean
