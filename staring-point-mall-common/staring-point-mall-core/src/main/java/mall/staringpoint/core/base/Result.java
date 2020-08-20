bf69fd192d7fce815787ce638a0998d4bd2fdab1
FengJie <2109921940@qq.com>
2020-08-20 23:04:45 +0800

    1. 优化

--- staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/base/Result.java
+++ staring-point-mall-common/staring-point-mall-core/src/main/java/mall/staringpoint/core/base/Result.java
@@ -1,6 +1,9 @@
 package mall.staringpoint.core.base;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -10,6 +13,9 @@ import java.io.Serializable;
  * @Description: 基础响应实体
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Result<T> implements Serializable {
 
     /**
